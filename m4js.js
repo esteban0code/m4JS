465ac704d5c816182bca1cb23db340bebd0475ae
esteban.dosztal <esteban.dosztal@morganstanley.com>
2013-09-28 00:19:47 -0400

    0.0 - sumOfDigits

---
+++ m4js.js
@@ -0,0 +1,41 @@
+var M4JS = (function() {
+	var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+	// private dta structure PairOfDigits
+	function PairOfDigits(n1, n2) {
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+
+	PairOfDigits.prototype.toString = function() {
+		return ('{ n1: ' + this.n1 + ', n2: ' + this.n2 + ' }');
+	};
+
+
+	var sumOfDigits = (function() {
+		var pairs = {};
+		var l = digits.length;
+		for (var i = 1; i < l; i++) {
+			for (var j = i; j < l; j++) {
+				var pair = new PairOfDigits(i, j);
+				var sum = (i + j) + '';
+				pairs[pair] = sum;
+			}
+		}
+
+		return pairs;
+	})();
+
+
+
+
+	return {
+		sumOfDigits: sumOfDigits,
+		PairOfDigits: PairOfDigits
+	};
+})();
+
+for (var pair in M4JS.sumOfDigits) {
+	console.log('pair: ' + pair);
+	console.log('M4JS.sumOfDigits[pair]: ' + M4JS.sumOfDigits[pair]);
+}
